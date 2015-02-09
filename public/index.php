177790b4e28978c410a55e4244fe2fd660000c18
John Doe <gamefake@live.nl>
2015-02-09 14:58:00 +0100

    verder met public/.htaccess

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php 
+	echo "Dit is de index.php pagina"; 
+	
+	if (isset($_GET['url']))
+	{
+		$url = $_GET['url'];
+	}
+	else
+	{
+		$url = "homePage";
+	}
+?>
+
+<h3>Index</h3>
+get = <?php echo $url; ?>
